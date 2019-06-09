95e68c086368a7b801339de367fd56fd534f8322
etrimbs <eric.trimo@gmail.com>
2019-06-09 14:57:38 -0400

    finish layout, routes, add signup stub

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do    # For details on the DSL available within th
 
   root 'static_pages#home'
 
-  get 'static_pages/home'
+  get '/helf',    to:'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/help',    to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'  
 
-  get 'static_pages/about'
-
-  get 'static_pages/help'
-
-  get 'static_pages/contact'  
+  get '/signup',  to: 'users#new'
 
 end

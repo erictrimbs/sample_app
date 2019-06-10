be38a6cc07c10433f5da268cbc8ffda7a909c950
etrimbs <eric.trimo@gmail.com>
2019-06-09 22:05:48 -0400

    Added validation to name, email. Not complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+                        # or /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+    validates :name,  presence: true, length: {maximum: 50}
+    
+    validates :email, presence: true, length: {maximum: 255}, 
+                      format: {with: VALID_EMAIL_REGEX}, 
+                      uniqueness: {case_sensitive: false}
+
 end

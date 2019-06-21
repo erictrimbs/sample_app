832530152ca557e5f10317a14082c705dfd5e7a8
etrimbs <eric.trimo@gmail.com>
2019-06-21 13:48:19 -0400

    Working on edit user information via update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,18 @@ class UsersController < ApplicationController
     #debugger #to debug from server console
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
